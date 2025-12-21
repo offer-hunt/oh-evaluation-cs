4d76dd4364b7347a6b339c9592633ed8a2ee6852
bogdanchik <ribehadance@gmail.com>
2025-12-22 06:28:22 +0700

    поправил api для learning

--- Oh.Evaluation/Oh.Evaluation.Api/ApiClients/ILearningClient.cs
+++ Oh.Evaluation/Oh.Evaluation.Api/ApiClients/ILearningClient.cs
@@ -5,6 +5,6 @@ namespace Oh.Evaluation.Api.ApiClients;
 
 public interface ILearningClient
 {
-    [Post("/api/learning/ratings/{courseId}}")]
+    [Post("/api/v1/internal/upsert-result")]
     public Task InsertEvaluationResult(Guid courseId, [Body] QuestionStateUpsertRequest request);
 }
